fbbf2e0ed13878e7505c005087556af0ff6443bb
ian-w <9qfr69ut@duck.com>
2024-07-05 08:26:53 +0800

    Delete useless file.

--- scripts/scp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-scp custom_rules.rb root@192.168.100.1:/etc/openclash/rule_provider/scripts
-scp refresh_rules.sh root@192.168.100.1:/etc/openclash/rule_provider/scripts
-scp rulesets_scripts.sh root@192.168.100.1:/etc/openclash/rule_provider/scripts
-scp update_rules.sh root@192.168.100.1:/etc/openclash/rule_provider/scripts
